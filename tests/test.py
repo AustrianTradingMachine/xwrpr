a84b2e788bdbcef946def8b8707de219de001aa4
ATM <craighero.philipp@gmail.com>
2024-07-03 08:12:59 +0200

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,24 +1,25 @@
 from config import XTB, generate_logger
 import time
+
 DEMO=True
 
 logger=generate_logger(name="TEST",path='/home/philipp/Trading/XTB/Logger')
 
-print(XTB.WRAPPER_VERSION)
-print(XTB.API_VERSION)
+#print(XTB.WRAPPER_VERSION)
+#print(XTB.API_VERSION)
 
 
 XTBData=XTB.Wrapper(demo=DEMO, logger=logger)
 
-#version=XTBData.getVersion()
+version=XTBData.getVersion()
 
-#print('major:', version['major'])
-#print('minor:', version['minor'])
-#print('patch:', version['patch'])
+print('major:', version['major'])
+print('minor:', version['minor'])
+print('patch:', version['patch'])
 
-#th=XTBData.getTradingHours(symbols=['EURUSD'])
+th=XTBData.getTradingHours(symbols=['EURUSD'])
 
-#print(th)
+print(th)
 
 XTBData.getCandles(symbol='EURUSD')
 
