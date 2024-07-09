302f22c45af44fc2aea24195d9394e48dcd6caaf
AustrianTradingMachine <craighero.philipp@gmail.com>
2024-07-09 21:29:21 +0200

    new file:   MANIFEST.in 	new file:   install_home.py 	modified:   pyproject.toml 	modified:   setup.py 	renamed:    XTB/__init__.py -> src/XTB/__init__.py 	renamed:    XTB/account.py -> src/XTB/account.py 	renamed:    XTB/api.cfg -> src/XTB/api.cfg 	renamed:    XTB/client.py -> src/XTB/client.py 	renamed:    XTB/handler.py -> src/XTB/handler.py 	renamed:    XTB/utils.py -> src/XTB/utils.py 	renamed:    XTB/wrapper.py -> src/XTB/wrapper.py 	modified:   tests/config.py 	new file:   user.cfg

--- setup.py
+++ setup.py
@@ -1,39 +1,18 @@
 from setuptools import setup, find_packages
-import pathlib
+from install_home import CustomInstall
+from pathlib import Path
 
-here = pathlib.Path(__file__).parent.resolve()
-long_description = (here / "README.md").read_text(encoding="utf-8")
-
-VERSION = '0.1.0'
-API = '2.5.0'
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name="XTBpy",
-    version=VERSION,
-    api_version=API
-    author="Philipp Craighero",
-    author_email="",
-    description="A wrapper for the XTB API",
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    keywords="XTB,API,trading,finance,development",
-    url='https://github.com/AustrianTradingMachine/XTBpy',
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    python_requires=">=3.7, <4",
-    install_requires=['pandas','pytz','tzlocal'],
-    license='GPLv3+',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Financial and Insurance Industry",
-        "Intended Audience :: Science/Research",
-        "Topic :: Office/Business :: Financial",
-        "Topic :: Office/Business :: Financial :: Investment",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Information Analysis",
-    ],
+    package_data={
+        'XTBpy': ['config/user.cfg']
+        },
+    cmdclass={
+        'install': CustomInstall,
+    }
 )
--- tests/config.py
+++ tests/config.py
@@ -3,5 +3,5 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-import XTB
-from XTB.utils import generate_logger
+from scr import XTB
+from src.XTB.utils import generate_logger
