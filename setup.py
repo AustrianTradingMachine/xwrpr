4d718c5236432d107f5b8677388a4af8750ce721
AustrianTradingMachine <craighero.philipp@gmail.com>
2024-07-09 22:21:17 +0200

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 from setuptools.command.install import install
 from setuptools import setup, find_packages
 from pathlib import Path
@@ -10,35 +9,21 @@ class CustomInstall(install):
     Custom installation class that extends the functionality of the base `install` class.
     This class creates a custom installation process for the XTBpy package.
     """
-
     def run(self):
-        """
-        Run the custom installation process.
-
-        This method ensures that the parent directory exists, copies the default config file to the destination,
-        and then calls the standard install process.
-
-        Raises:
-            OSError: If there is an error creating the parent directory or copying the config file.
-        """
-        # Ensure the parent directory exists
-        config_dir = os.path.expanduser("~/.XTBpy")
-        if not os.path.exists(config_dir):
-            os.makedirs(config_dir)
+        # Call the superclass run method
+        install.run(self)
         
-        # Path to the default config file within the package
-        default_config = os.path.join(os.path.dirname(__file__), 'XTBpy', 'user.cfg')
+        # Define the configuration directory and file paths
+        config_dir = os.path.expanduser('~/.XTBpy')
+        config_file_path = os.path.join(config_dir, 'user.cfg')
         
-        # Destination path
-        destination_config = os.path.join(config_dir, 'user.cfg')
+        # Ensure the configuration directory exists
+        os.makedirs(config_dir, exist_ok=True)
         
-        # Copy the default config file to the destination
-        if not os.path.exists(destination_config):  # To avoid overwriting an existing file
-            shutil.copy(default_config, destination_config)
-        
-        # Call the standard install process
-        super().run(self)
-
+        # Check if the config file already exists to avoid overwriting
+        if not os.path.exists(config_file_path):
+            with open(config_file_path, 'w') as config_file:
+                config_file.write("# Your default configuration\n")
 
 
 this_directory = Path(__file__).parent
