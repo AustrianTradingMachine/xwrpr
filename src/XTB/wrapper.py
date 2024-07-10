7a2098a0c1e03e902ff4888fc5cbe59560bda93f
ATM <craighero.philipp@gmail.com>
2024-07-10 12:56:22 +0200

    Update wrapper.py

--- src/XTB/wrapper.py
+++ src/XTB/wrapper.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8; py-indent-offset:4 -*-
 ###########################################################################
 #
+#    XTBpy, a wrapper for the API of XTB (https://www.xtb.com)
+#
 #    Copyright (C) 2024  Philipp Craighero
 #
 #    This program is free software: you can redistribute it and/or modify
