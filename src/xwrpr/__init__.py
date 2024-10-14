57142fd1beaa2b7a8a066dc27f4c8f94ff293333
Philipp <craighero.philipp@gmail.com>
2024-10-14 16:42:18 +0000

    modified:   src/xwrpr/__init__.py

--- src/xwrpr/__init__.py
+++ src/xwrpr/__init__.py
@@ -21,8 +21,28 @@
 #
 ###########################################################################
 
+"""
+xwrpr - A wrapper for the API of XTB (https://www.xtb.com)
+
+This module provides a Wrapper class to interact with the XTB API.
+"""
+
+import sys
+from importlib.metadata import version, PackageNotFoundError
 from xwrpr.wrapper import Wrapper
 
+# Ensure the script is being run with Python 3.9 or higher
+if sys.version_info < (3, 9):
+    raise RuntimeError("xwrpr requires Python 3.9 or higher")
+
+# Define the XTB Api version the xwrpr relies on
 API_VERSION = '2.5.0'
-__version__ = '1.0.0'
+
+# Get the version dynamically from the package metadata
+try:
+    __version__ = version("xwrpr")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+# Define what is exported when `from xwrpr import *` is used
 __all__ = ['Wrapper', 'API_VERSION']
